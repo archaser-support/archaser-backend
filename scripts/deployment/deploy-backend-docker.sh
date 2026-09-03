3e82d15de50b6262a0fe3907cbd4efcb7f11251e
Nilotpal Bose <nilotpal@archaser.com>
2026-09-03 18:14:54 +0530

    fix(deployment): reintroduce cron-jobs build step in backend Docker deployment script
    This commit adds back the build step for the @archaser/cron-jobs package in the deploy-backend-docker.sh script, ensuring that it is included in the build process when SKIP_BUILD is not set to true. This change aims to maintain consistency in the deployment workflow.

--- scripts/deployment/deploy-backend-docker.sh
+++ scripts/deployment/deploy-backend-docker.sh
@@ -307,8 +307,8 @@ if [[ "$SKIP_BUILD" != "true" ]]; then
     npm run build -w @archaser/auth
     npm run build -w @archaser/sms-send
     npm run build -w @archaser/credit-insurance-domain
-    npm run build -w @archaser/cron-jobs
     npm run build -w @archaser/billing-connector
+    npm run build -w @archaser/cron-jobs
     npm run build -w @archaser/api
     npm run build -w @archaser/worker
     npm run build -w @archaser/sms
